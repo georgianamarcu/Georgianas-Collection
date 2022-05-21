bc71a6b2464db9265f5eaac1fbdeb86485274994
Georgiana Marcu <georgiana.marcu22@gmail.com>
2022-05-21 17:28:17 +0200

    Movie collection page

--- display:hide-cards.js
+++ display:hide-cards.js
@@ -1,18 +1,16 @@
-let hideCards = () =>{
-    let ul = document.querySelector('ul').children
-    let ulArr = Array.from(ul);
-    ulArr.forEach((li) =>{
-        
-        li.style.display = "none";
-    
-    })
-    }
-    
-    let showCards = (myarray) =>{ 
-        console.log(myarray)  
-        myarray.forEach((li) =>{   
-            li.parentNode.style.display = "block";    
-        })
-        }
+let hideCards = () => {
+  let ul = document.querySelector("ul").children;
+  let ulArr = Array.from(ul);
+  ulArr.forEach((li) => {
+    li.style.display = "none";
+  });
+};
 
-        export {hideCards,showCards}
+let showCards = (myarray) => {
+  console.log(myarray);
+  myarray.forEach((li) => {
+    li.parentNode.style.display = "block";
+  });
+};
+
+export { hideCards, showCards };
